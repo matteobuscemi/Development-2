9b7b81db1bfa79463b3a83168f077b18a9a9967e
matteobuscemi <matteo.buscemi@student.ehb.be>
2020-05-09 19:20:26 +0200

    Klaar grotendeels met script.js

--- js/script.js
+++ js/script.js
@@ -20,9 +20,29 @@ $(document).ready(function(){
         console.log(valcountry);
         printData(valcountry);
 
-        $("div.minimized").click(function(){
-           var thisDiv = $(this).parent().attr("id");
-            $("div#body-"+ thisDiv).slideToggle();
+    $("div.delete").click(function(){
+        $(this).parent().parent().remove();
+        console.log("Blablabla");
+
+        
+    })
+         $("div.minimized").click(function(){
+           var thisDiv = $(this).attr("id");
+           var thisDivClass = $(this).parent().attr("class");
+           var divI = $(this).children().attr("class");
+           console.log(divI);
+            if (divI == "fas fa-minus"){
+                
+                $("div#"+thisDiv+" i.fa-minus").attr("class","fas fa-chevron-down");
+                $("div#body-"+ thisDiv).slideUp();
+                console.log("Test");
+            }
+            else {
+                $("div#"+thisDiv+" i.fa-chevron-down").attr("class","fas fa-minus");
+                $("div#body-"+ thisDiv).slideDown();
+                console.log("Test");
+            }
+
             });
         });
 
@@ -36,13 +56,13 @@ function printData(val){
                     console.log(country);
 
                     var html = $(`<div class="country-maximized">
-                    <div class="country-header" id="${country.Country}">
+                    <div class="country-header" id="${country.CountryCode}">
                 <div class="delete"><i class="fas fa-trash"></i></div>
                 <div class="flag"><img src="../flags/${country.CountryCode.toLowerCase()}.svg"></div>
                 <h1>${country.Country}</h1>
-                <div class="minimized"><i class="fas fa-minus"></i></div>
+                <div class="minimized" id="${country.CountryCode}"><i class="fas fa-minus"></i></div>
                     </div>
-                    <div class="country-body" id="body-${country.Country}">
+                    <div class="country-body" id="body-${country.CountryCode}">
                 <table>
                     <tr>
                         <th class="title">New confirmed</th>
